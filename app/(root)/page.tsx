a79c0548446921ada8326b9c1c489065d2cc0ca0
khairul-islam-shaon <shaonkhairul@gmail.com>
2025-08-22 15:47:17 +0600

    product-lists

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/smaple-product";
 
 export default function Home() {
   return (
     <div>
-    
+    <ProductList data={sampleData.products} title="Featured Products"/>
     </div>
   );
 }
