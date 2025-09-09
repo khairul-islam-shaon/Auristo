d42b2e61aa71b4937c01ca80360c54126146973c
khairul Islam <khairul.islam@adndiginet.com>
2025-09-09 16:44:25 +0600

    feat: enhance JWT and session callbacks to include user role and name handling

--- auth.ts
+++ auth.ts
@@ -58,12 +58,36 @@ export const config = {
   callbacks: {
     async session({ session, user, trigger, token }: any) {
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
+
+      console.log(token);
 
       if (trigger === "update") {
         session.user.name = user;
       }
       return session;
     },
+
+    // jwt
+
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if(user){
+        token.role = user.role;
+        // if user has no name then use the email
+        if(user.name === "NO_NAME"){
+          token.name = user.email!.split("@")[0];
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where:{id:user.id},
+            data:{name:token.name}
+          })
+
+        }
+      }
+      return token;
+    }
   },
 } satisfies NextAuthConfig;
 
